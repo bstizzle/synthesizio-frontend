8d011804950edb66ca16af6284d45419c9b7aded
bstizzle <whereyouben@gmail.com>
2021-02-03 17:20:50 -0500

    added env file and set up initial fetch

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,7 +5,16 @@ import SynthEditor from './SynthEditor';
 import UserPage from './UserPage';
 
 function App() {
+  const [users, setUsers] = useState([])
 
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/users`)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data)
+        setUsers(data)
+      })
+  }, [])
 
   return (
     <div>

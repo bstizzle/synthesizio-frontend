5fe06f1f3aa50b30edce0b054b59d0f5fda0079d
bstizzle <whereyouben@gmail.com>
2021-02-08 16:04:16 -0500

    added typography to login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 function Login({ users, onSetUsers, onSetCurrentUser }) {
     const [username, setUsername] = useState("")
@@ -78,7 +79,9 @@ function Login({ users, onSetUsers, onSetCurrentUser }) {
             <Grid container spacing={2}>
                 <Grid item xs={12}>
                     <Paper className={classes.paper}>
-                        <h1>Welcome to Synthesiz.io!</h1>
+                        <Typography variant="h1">
+                            Welcome to Synthesiz.io!
+                        </Typography>
                     </Paper>
                 </Grid>
                 <Grid item xs={6}>

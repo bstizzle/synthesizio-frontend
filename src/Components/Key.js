adadef69b3373af58a8a91fdc7cba3ea5161e5d2
bstizzle <whereyouben@gmail.com>
2021-02-09 13:23:53 -0500

    added octave markings to keys

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -4,15 +4,37 @@ function Key({note, frequency, index}) {
     const twelfthTwo = Math.pow(2, 1/12);
     const keyFreq = Math.round((27.5 * Math.pow(twelfthTwo, index)) * 100)/100
 
+    //key rendering details
     let noteLabel;
     let noteAccents;
+    let octave;
 
     if(note.length === 1){
         noteLabel = note;
     }else{
         noteAccents = note.split("/")[0]
     }
+
+    if(index < 12){
+        octave = 0;
+    }else if(index < 24){
+        octave = 1;
+    }else if(index < 36){
+        octave = 2;
+    }else if(index < 48){
+        octave = 3;
+    }else if(index < 60){
+        octave = 3;
+    }else if(index < 72){
+        octave = 4
+    }else if(index < 84){
+        octave = 5;
+    }else if(index < 96){
+        octave = 6;
+    }
+    //end key rendering details
   
+    //event listeners
     function keyPressed(event) {
         if (event.buttons & 1) {
             let dataset = event.target.dataset;
@@ -33,11 +55,14 @@ function Key({note, frequency, index}) {
           console.log(dataset)
         }
     }
+    //end event listeners
 
     return(
         <div className="key" data-id={index} onMouseDown={keyPressed} onMouseUp={keyReleased} >
             <div className="key-label">
                 {noteLabel === note ? noteLabel : noteAccents}
+                <br></br>
+                <sub>{octave}</sub>
             </div>
         </div>
     )

6a61a19aa594a8ad1e8239fb9a0eb9e78abee5a6
bstizzle <whereyouben@gmail.com>
2021-02-10 14:16:13 -0500

    added descriptions for each param's modal

--- src/Components/InfoModal.js
+++ src/Components/InfoModal.js
@@ -22,6 +22,8 @@ function InfoModal({ topic }) {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
+            maxWidth: '50%',
+            margin: 'auto'
         },
         paper: {
             backgroundColor: theme.palette.background.paper,
@@ -32,6 +34,14 @@ function InfoModal({ topic }) {
     }));
       
     const classes = useStyles();
+
+    //descriptions for each of the info popups
+    const paramInfo = {
+        "Keyboard": "Click on keys to play your synth at that pitch, the first oscillator frequency will change to the key you click, with the second frequency mainting its interval above or below.",
+        "Distortion": "Distortion creates a non-linear relationship between the input and the output of a sound. It does this by imposing another waveform on top of your sound, the more vertical the curve the more harsh the distortion. Distortion Gain determines the extremes of the change. Softer distortion provides more natural sound, while harder introduces more clipping/peaking. As an example, the default soft distortion curve used can be viewed here: https://www.desmos.com/calculator/jsqwtqac8p",
+        "Oscillators": "The oscillators produce your basic sound. You can select between four waveforms, which you can view in the Oscilliscope setting of the visualiser. You can set the overall gain (volume) for both oscillators, as well as set the frequency (or note/tone) of each oscillator separately.",
+        "Audio Visualiser": "The Audio Visualiser turns your sound into visual feedback. The EQ (or 'equalizer') setting shows you a graph of the volume of the individual frequencies of the sound, lowest pitch on the left and highest on the right. The Oscilliscope setting shows you the final output waveform itself."
+    }
 
     return(
         <div>
@@ -47,7 +57,7 @@ function InfoModal({ topic }) {
                 }}>
                 <Fade in={open}>
                     <div className={classes.paper}>
-                        Informative Text about {topic}
+                        {paramInfo[topic]}
                     </div>
                 </Fade>
             </Modal>

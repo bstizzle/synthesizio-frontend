d76d4ecdd14baf2523c2a3ebadc2fbeef5b6910c
bstizzle <whereyouben@gmail.com>
2022-05-31 14:44:42 -0400

    wip

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -1,12 +1,42 @@
 import React from 'react'
-import OscUtils from './OscUtils'
+import { getOscInterval, getSecondFreq } from './OscUtils'
 
-function Key({ note, frequency, index, freq1, onChangeFreq1, onChangeFreq2 }) {
+function Key({ note, frequency, index, freq1, freq2, onChangeFreq1, onChangeFreq2 }) {
+    let noteLabel;
+    let noteAccents;
+    let octave;
+    let keyClass = "white-key";
     
-    const octave = OscUtils.octave;
-    const noteLabel = OscUtils.noteLabel;
-    const keyClass = OscUtils.keyClass;
-    const noteAccents = OscUtils.noteAccents;
+    if(note.includes("C#") || note.includes("D#") || note.includes("F#") || note.includes("G#") || note.includes("A#")){
+        keyClass = "black-key";
+    }
+    
+    if(note.length === 1){
+        noteLabel = note;
+    }else{
+        noteAccents = note.split("/")[0]
+    }
+    
+    if(index < 12){
+        octave = 0;
+    }else if(index < 24){
+        octave = 1;
+    }else if(index < 36){
+        octave = 2;
+    }else if(index < 48){
+        octave = 3;
+    }else if(index < 60){
+        octave = 3;
+    }else if(index < 72){
+        octave = 4
+    }else if(index < 84){
+        octave = 5;
+    }else if(index < 96){
+        octave = 6;
+    }
+    //end key rendering details
+
+    const oscInterval = getOscInterval(freq1, freq2)
 
     //event listeners
     function keyPressed(event) {
@@ -17,7 +47,7 @@ function Key({ note, frequency, index, freq1, onChangeFreq1, onChangeFreq2 }) {
                 dataset["pressed"] = "yes";
                 console.log(dataset)
                 console.log(freq1)
-                let secondFreq = OscUtils.getSecondFreq(index)
+                let secondFreq = getSecondFreq(oscInterval, index)
                 console.log(secondFreq)
                 onChangeFreq1(frequency)
                 onChangeFreq2(secondFreq)
